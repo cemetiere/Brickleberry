25b8dea0d84d267bdc1632725c2e9691a5106e9c
cemetiere <kirodron@mail.ru>
2023-12-11 17:37:36 +0300

    fixes

--- indexes.sql
+++ indexes.sql
@@ -1,5 +1,5 @@
-CREATE INDEX person_id_index ON Inspectors(person_id) USING HASH; 
-CREATE INDEX brigade_id_index ON Inspectors(brigade_id) USING HASH; 
-CREATE INDEX animal_count_index ON Animal_type_territory(animal_count) USING BTREE;
-CREATE INDEX warehouse_id_index ON Resourse(warehouse_id) USING HASH;
-CREATE INDEX resource_count_index ON Resource(count) USING BTREE; 
+CREATE INDEX person_id_index ON Inspectors USING HASH(person_id); 
+CREATE INDEX brigade_id_index ON Inspectors USING HASH(brigade_id); 
+CREATE INDEX animal_count_index ON Animal_type_territory USING BTREE(animal_count);
+CREATE INDEX warehouse_id_index ON Resourse USING HASH(warehouse_id);
+CREATE INDEX resource_count_index ON Resource USING BTREE(count); 
